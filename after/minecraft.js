f314bdaafeb0b5c75dc989b6117d32719e9bde4a
JessicaDamasceno <jessidamasceno@gmail.com>
2022-02-21 21:00:09 -0300

    Trocando as pastas

--- after/minecraft.js
+++ after/minecraft.js
@@ -1,33 +1,27 @@
-function mostrar(){  
-    let numero = document.getElementById('escolha');
-    let tela = document.querySelector('canvas');
-    let pincel = tela.getContext('2d');
+let numero = document.querySelector(".numero");
+let tela = document.querySelector("canvas");
+let pincel = tela.getContext("2d");
 
-    if(Number(numero.value) == 1){
-        clear();
-        mostrarCreeper();
-        
-    }else if(Number(numero.value) == 2){
-        clear();
-        mostrarZumbi();
-
-    }else if(Number(numero.value) == 3){
-        clear();
-        mostrarSteve();
-        
-    }else if(Number(numero.value) == 4){
-        clear();
-        mostrarAlex();
-    
-    }else{
-        alert('Número inválido!!');
-        clear();
-    } 
-
-    function clear (){
-        pincel.fillStyle = 'white';
-        pincel.fillRect(0,0,tela.width,tela.height);
-        numero.value = '';
-        numero.focus();
-    }
+function clear() {
+	pincel.clearRect(0, 0, tela.width, tela.height);
+	numero.value = "";
+	numero.focus();
+}
+function mostra() {
+	if (numero.value == 1) {
+		clear();
+		mostraCreeper();
+	} else if (numero.value == 2) {
+		clear();
+		mostraZumbi();
+	} else if (numero.value == 3) {
+		clear();
+		mostraSteve();
+	} else if (numero.value == 4) {
+		clear();
+		mostraAlex();
+	} else {
+		clear();
+		alert("Número inválido!! Tente novamente.");
+	}
 }
